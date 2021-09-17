401ce966667e56b452f946837019c6ff5a437abd
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2021-09-17 14:49:45 +0000

    Tidying up to complete running of App. Needed to add 3 coastal functions to sub-folder in MRBreach to avoid having to include the full set of coastal functions.
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/MRBreach@38 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- help/html/open_manual.m
+++ help/html/open_manual.m
@@ -2,5 +2,5 @@ function open_manual()
 %find the location of the asmita app and open the manual
 appinfo = matlab.apputil.getInstalledAppInfo;
 idx = find(strcmp({appinfo.name},'MRBreach'));
-fpath = [toolboxes(idx(1)).location,'/doc/MRBreach manual.pdf'];
+fpath = [appinfo(idx(1)).location,'/doc/MRBreach manual.pdf'];
 open(fpath)
