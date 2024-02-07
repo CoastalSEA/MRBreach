f8c5dcfd0bc3dedee9631974526243436257b904
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2024-02-07 19:19:49 +0000

    Modified the Help menu to access Documentation and Manual from dropdown. Asmita Training updated to be the same as 2024 version using v3.5 (not checked with v4.0 yet).
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/MRBreach@227 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- MRBreach.m
+++ MRBreach.m
@@ -121,7 +121,8 @@ classdef MRBreach < muiModelUI
             menu.Analysis(1).Callback = repmat({@obj.analysisMenuOptions},[1,2]);
             
             %% Help menu --------------------------------------------------
-            menu.Help(1).Callback = {@obj.Help}; %make model specific?
+            menu.Help.List = {'Documentation','Manual'};
+            menu.Help.Callback = repmat({@obj.Help},[1,2]);
             
         end
         
@@ -243,10 +244,15 @@ classdef MRBreach < muiModelUI
         end
 
         %% Help menu ------------------------------------------------------
-        function Help(~,~,~)
-            doc mrbreach                               % << Edit to documentation name if available
-        end
-
+        function Help(~,src,~)
+            %menu to access online documentation and manual pdf file
+            switch src.Text
+                case 'Documentation'
+                    doc mrbreach   %must be name of html help file  
+                case 'Manual'
+                    mrb_open_manual;
+            end
+        end  
         %% Check that toolboxes are installed------------------------------
         function isok = check4muitoolbox(~)
             %check that dstoolbox and muitoolbox have been installed
